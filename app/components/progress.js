67e9f6eee0355a350423a8e0aaadf4b9cd45bdef
adarsh tripathi <adarshtripathi262@gmail.com>
2026-05-30 09:52:04 +0530

    change in mibile response

--- app/components/progress.js
+++ app/components/progress.js
@@ -8,7 +8,7 @@ const Progress = () => {
   const days = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
   return (
-    <div className="absolute right-0">
+    <div className="absolute -right-5">
       <motion.div 
         initial={{ opacity: 0, scale: 0.95 }}
         animate={{ opacity: 1, scale: 1 }}
